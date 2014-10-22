b4ffd34cde276850a05ee81074d90e41d88f0363
MartinusPL <mrcngrzsk@gmail.com>
2014-10-22 18:47:22 +0200

    Initial commit

---
+++ DataGenerator/Classes.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HD
+{
+    public class Pracownik : IComparable
+    {
+        public string PESEL { get; set;}
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int ID_funkcji { get; set;}
+        public string Specjalnosc { get; set; }
+        public Pracownik() { }
+        public Pracownik(string pPESEL, string pImie, string pNazwisko, int pID_funkcji, string pSpecjalnosc) 
+        {
+            this.PESEL = pPESEL;
+            this.Imie = pImie;
+            this.Nazwisko = pNazwisko;
+            this.ID_funkcji = pID_funkcji; 
+            this.Specjalnosc = pSpecjalnosc;
+        }
+
+        public override string ToString()
+        {
+            return PESEL + " " + Imie + " " + Nazwisko + " " + ID_funkcji + " " + Specjalnosc;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var p = (Pracownik) obj;
+            return String.Compare(p.PESEL, this.PESEL, System.StringComparison.Ordinal);
+        }
+    }
+    public class Rozmowa
+    {
+        public int ID_rozmowa { get; set; }
+        public int PESEL { get; set; }
+        public DateTime Data  { get; set; }
+        public int Czas_trwania { get; set; }
+        public int Nr_telefonu { get; set; }
+        public Rozmowa() { }
+        public Rozmowa(int pID_rozmowa, int pPESEL, DateTime pData, int pCzas_trwania, int pNr_telefonu)
+        {
+            this.ID_rozmowa = pID_rozmowa;
+            this.PESEL = pPESEL;
+            this.Data = pData;
+            this.Czas_trwania = pCzas_trwania;
+            this.Nr_telefonu = pNr_telefonu;
+        }
+
+    }
+    public class Pojazd : IComparable
+    {
+        public string Nr_rejestracyjny { get; set; }
+        public string Model { get; set; }
+        public string Marka { get; set; }
+        public bool Aktywny { get; set; }
+        public Pojazd(){}
+        public Pojazd(string pNr_rejestracyjny, string pModel, string pMarka, bool pAktywny) 
+        {
+            this.Nr_rejestracyjny = pNr_rejestracyjny;
+            this.Model = pModel;
+            this.Marka = pMarka;
+            this.Aktywny = pAktywny;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var p = (Pojazd) obj;
+            return System.String.Compare(Nr_rejestracyjny, p.Nr_rejestracyjny, System.StringComparison.Ordinal);
+        }
+
+        public override string ToString()
+        {
+            return Nr_rejestracyjny + " " + Marka + " " + Model + " " + Aktywny;
+        }
+    }
+    public class Zespol 
+    {
+        public int ID_zespolu { get; set; }
+        public int ID_rodzaj { get; set; }
+        public string ID_pojazd { get; set; }
+        public DateTime Rozpoczecie_dyzuru { get; set; }
+        public DateTime Zakonczenie_dyzuru { get; set; }
+        public string Zajety { get; set; } 
+        public Zespol() { }
+        public Zespol(int pID_zespolu, int pID_rodzaj, string pID_pojazd, DateTime pRozpoczecie_dyzuru, DateTime pZakonczenie_dyzuru, string pZajety) 
+        {
+            this.ID_zespolu = pID_zespolu;
+            this.ID_rodzaj = pID_rodzaj;
+            this.ID_pojazd = pID_pojazd;
+            this.Rozpoczecie_dyzuru = pRozpoczecie_dyzuru;
+            this.Zakonczenie_dyzuru = pZakonczenie_dyzuru;
+            this.Zajety = pZajety;
+        }
+    }
+    public class Wyjazd 
+    {
+        public int ID_wyjazdu { get; set; }
+        public int ID_zespolu { get; set; }
+        public int ID_rozmowy { get; set; }
+        public DateTime Data { get; set; }
+        public int ID_kategorii { get; set; }
+        public int Kod_wyjazdu { get; set; }
+        public string Adres_miasto { get; set; }
+        public string Adres_ulica { get; set; }
+        public string Adres_nr_domu { get; set; }
+        public string Adres_nr_lokalu { get; set; }
+        public string Dodatkowe_informacje { get; set; }
+        public Wyjazd() { }
+        public Wyjazd( int pID_wyjazdu, int pID_zespolu, int pID_rozmowy, DateTime pData, int pID_kategorii, int pKod_wyjazdu, string pAdres_miasto, string pAdres_ulica,string pAdres_nr_domu, string pAdres_nr_lokalu, string pDodatkowe_informacje) 
+        {
+            this.ID_wyjazdu = pID_wyjazdu;
+            this.ID_zespolu = pID_zespolu;
+            this.ID_rozmowy = pID_rozmowy;
+            this.Data = pData;
+            this.ID_kategorii = pID_kategorii;
+            this.Adres_miasto = pAdres_miasto;
+            this.Adres_ulica = pAdres_ulica;
+            this.Adres_nr_domu = pAdres_nr_domu;
+            this.Adres_nr_lokalu = pAdres_nr_lokalu;
+            this.Dodatkowe_informacje = pDodatkowe_informacje;
+        
+        }
+    }
+    public class Przynaleznosc 
+    {
+        public int PESEL { get; set; }
+        public int ID_zespol { get; set; }
+        public Przynaleznosc() { }
+        public Przynaleznosc(int pPESEL, int pID_zespol) 
+        {
+            this.PESEL = pPESEL;
+            this.ID_zespol = pID_zespol;
+        }
+        
+    }
+
+    public class RodzajZespolu
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+
+        public RodzajZespolu() {}
+
+        public RodzajZespolu(int id, string nazwa)
+        {
+            this.Id = id;
+            this.Nazwa = nazwa;
+        }
+    }
+
+    public class KategoriaWyjazdu
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+
+        public KategoriaWyjazdu() {}
+        public KategoriaWyjazdu(int id, string nazwa)
+        {
+            this.Id = id;
+            this.Nazwa = nazwa;
+        }
+    }
+
+    public class FunkcjaPracownika
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+
+        public FunkcjaPracownika() { }
+        public FunkcjaPracownika(int id, string nazwa)
+        {
+            this.Id = id;
+            this.Nazwa = nazwa;
+        }
+    }
+}
---
+++ DataGenerator/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using HD;
+
+namespace DataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region slowniki
+            List < RodzajZespolu >  rodzajeZespolu;
+            rodzajeZespolu = new List<RodzajZespolu>()
+            {
+                new RodzajZespolu(1,"Podstawowy"),
+                new RodzajZespolu(2,"Specjalistyczny"),
+                new RodzajZespolu(3,"Transportowy"),
+                new RodzajZespolu(4,"Neonatalogiczny")
+            };
+
+            List<FunkcjaPracownika> funkcjePracownika;
+            funkcjePracownika = new List<FunkcjaPracownika>()
+            {
+                new FunkcjaPracownika(1, "Dyspozytor"),
+                new FunkcjaPracownika(2, "Ratownik kierowca"),
+                new FunkcjaPracownika(3, "Ratownik medyczny"),
+                new FunkcjaPracownika(4, "Ratownik medyczny kierowca"),
+                new FunkcjaPracownika(5, "Pielęgniarka"),
+                new FunkcjaPracownika(6, "Lekarz")
+            };
+
+            List<KategoriaWyjazdu> kategorieWyjazdow;
+            kategorieWyjazdow = new List<KategoriaWyjazdu>()
+            {
+                new KategoriaWyjazdu(1,"Wypadek komunikacyjny"),
+                new KategoriaWyjazdu(2,"Nagłe zatrzymanie krążenia"),
+                new KategoriaWyjazdu(3,"Poród"),
+                new KategoriaWyjazdu(4,"Transport medyczny"),
+                new KategoriaWyjazdu(5,"Bóle w klatce piersiowej"),
+                new KategoriaWyjazdu(6,"Złamania kończyn"),
+                new KategoriaWyjazdu(7,"Utrata przytomności"),
+                new KategoriaWyjazdu(8,"Krwotoki"),
+                new KategoriaWyjazdu(9,"Urazy"),
+                new KategoriaWyjazdu(10,"Inne")
+            };
+
+#endregion
+
+            #region parametry wejsciowe
+            int liczbaRekordow = 100;
+            //System.Console.Write("Wpisz liczbe rekordow: ");
+            //liczbaRekordow = System.Convert.ToInt32(System.Console.ReadLine());
+
+            DateTime datestart = System.Convert.ToDateTime("2014-01-01");
+            DateTime dateend = System.Convert.ToDateTime("2014-10-10");;
+            //System.Console.Write("Wpisz date poczatkowa (RRRR-MM-DD): ");
+            //datestart = System.Convert.ToDateTime(System.Console.ReadLine());
+            //System.Console.Write("Wpisz date koncowa (RRRR-MM-DD): ");
+            //dateend = System.Convert.ToDateTime(System.Console.ReadLine());
+            #endregion
+
+            var rand = new Random(Environment.TickCount);
+            var iloscPracownikow = rand.Next(50, 100);
+            var imiona = new List<string>(){"Adam","Ewa","Marek","Danuta","Kazimierz"};
+            var nazwiska = new List<string>(){"Kubale","Zawadzka","Jastrzębski","Ocetkiewicz","Turowski"};
+            var pracownicy = new HashSet<Pracownik>();
+            while (pracownicy.Count < iloscPracownikow)
+            {
+                var imie = imiona.ElementAt(rand.Next(imiona.Count));
+                var nazwisko = nazwiska.ElementAt(rand.Next(nazwiska.Count));
+                var funkcja = funkcjePracownika.ElementAt(rand.Next(funkcjePracownika.Count));
+                var pesel1 = rand.Next(10000, 99999);
+                var pesel2 = rand.Next(10000, 99999);
+                var pesel_str = pesel1.ToString() + pesel2.ToString() + rand.Next(0, 9).ToString();
+
+                var pracownik = new Pracownik() { ID_funkcji = funkcja.Id, Imie = imie, Nazwisko = nazwisko, PESEL = pesel_str };
+                pracownicy.Add(pracownik);
+            }
+
+            var iloscPojazdow = rand.Next(20, 30);
+            var marki = new List<string>() { "Opel", "Mercedes", "Ford", "Renault", "Audi", "Peugot" };
+            var modele = new List<string>() { "A3", "C7", "Kaktus", "Sprinter", "4", "5", "232", "512", "1024" };
+            var pojazdy = new HashSet<Pojazd>();
+            while (pojazdy.Count < iloscPojazdow)
+            {
+                var marka = marki.ElementAt(rand.Next(marki.Count));
+                var model = modele.ElementAt(rand.Next(modele.Count));
+                var aktywny = rand.Next(0, 10);
+                var aktywny_bool = (aktywny <= 8);
+                var nr_rej1 = "GD";
+                var nr_rej2 = rand.Next(10000, 99999);
+                var nr_rej = nr_rej1.ToString() + nr_rej2.ToString();
+
+                var pojazd = new Pojazd() {Nr_rejestracyjny = nr_rej, Marka = marka, Model = model, Aktywny = aktywny_bool};
+                pojazdy.Add(pojazd);
+
+                System.Console.WriteLine(pojazd);
+            }
+
+            var timeDelta = dateend - datestart;
+            var liczbaDni = timeDelta.Days;
+            var liczbaZespolow = 15*2*liczbaDni;
+
+            var zespoly = new HashSet<Zespol>();
+
+            DateTime start = datestart;
+            while (zespoly.Count < liczbaZespolow)
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    var idZespol = zespoly.Count + 1;
+                    var idPojazdu = pojazdy.ElementAt(rand.Next(pojazdy.Count));
+                    var rodzajZespolu = rodzajeZespolu.ElementAt(rand.Next(rodzajeZespolu.Count));
+
+                    var koniec = start + TimeSpan.FromHours(12);
+                    var zespol = new Zespol()
+                    {
+                        ID_pojazd = idPojazdu.Nr_rejestracyjny,
+                        ID_rodzaj = rodzajZespolu.Id,
+                        ID_zespolu = idZespol,
+                        Rozpoczecie_dyzuru = start,
+                        Zakonczenie_dyzuru = koniec
+                    };
+
+                    zespoly.Add(zespol);
+                }
+                start += TimeSpan.FromHours(12);
+            }
+        }
+    }
+}
